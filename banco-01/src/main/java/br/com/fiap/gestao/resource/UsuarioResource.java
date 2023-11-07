10f5577134516690a177c4e23ed01afd45b65d93
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 20:47:50 -0300

    Listar

---
+++ banco-01/src/main/java/br/com/fiap/gestao/resource/UsuarioResource.java
@@ -0,0 +1,57 @@
+package br.com.fiap.gestao.resource;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.fiap.gestao.exception.BadInfoException;
+import br.com.fiap.gestao.model.User;
+import br.com.fiap.gestao.service.UsuarioService;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
+
+@Path("/usuarios")
+public class UsuarioResource {
+//	criar construtor
+//importar service
+	
+	
+	private UsuarioService service;
+	
+	
+	public UsuarioResource() throws ClassNotFoundException, SQLException {
+		service = new UsuarioService();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> listar() throwsSQLException {
+		
+		
+		return null;
+		
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response cadastrar(User user, @Context UriInfo uri) throws SQLException {
+		
+		try {
+			service.Cadastrar(user);
+			UriBuilder builder = uri .getAbsolutePathBuilder().path(String.valueOf(user.getId()));
+			
+			return Response.created(builder.build()).build();
+		}catch (BadInfoException e){
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+		}
+		
+	}
+}
